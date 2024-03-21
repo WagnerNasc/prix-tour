2a0713ae51631381db39b541fdfa49fbe8642d14
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-03-20 22:25:40 -0300

    feat: removed files

--- packages/server/src/repositories/interfaces/customer-address-repository-interface.ts
+++
@@ -1,7 +0,0 @@
-import { CustomerAddress } from '@/use-cases/interfaces/customer-address-interface'
-import { Customer } from '@/use-cases/interfaces/customer-interface'
-
-export interface ICustomerAddressRepository {
-  findManySorted(): Promise<CustomerAddress[] | []>
-  create(customer: CustomerAddress): Promise<Customer>
-}
