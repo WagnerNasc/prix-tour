649eea420551fc29d965a8034f66e1caf4e8fd1e
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-03-21 17:08:49 -0300

    feat: added conditional deleted_at equal to null

--- packages/server/src/repositories/tourist-attraction.repository.ts
+++ packages/server/src/repositories/tourist-attraction.repository.ts
@@ -1,6 +1,8 @@
 import { Pool } from 'pg'
 import { TouristAttraction } from '../use-cases/interfaces/tourist-attraction-interface'
 import { ITouristAttractionRepository } from './interfaces/tourist-attraction-repository-interface'
+import { Optional } from '@helpers/optional'
+import { OmitProps } from '@helpers/omit'
 
 export class TouristAttractionRepository
   implements ITouristAttractionRepository
@@ -14,7 +16,14 @@ export class TouristAttractionRepository
   async findById(id: string): Promise<{ data: TouristAttraction } | null> {
     try {
       const queryResult = await this.pool.query(
-        /* sql */ `SELECT * FROM tourist_attraction WHERE id = $1`,
+        /* sql */ `
+          SELECT 
+            * 
+          FROM 
+            tourist_attraction 
+          WHERE id = $1 AND   
+          deleted_at IS NULL
+        `,
         [id],
       )
 
@@ -48,11 +57,12 @@ export class TouristAttractionRepository
         FROM
           tourist_attraction
       `
-
+      // TODO: SQL Injection
       if (filter) {
         query += /* sql */ `
           WHERE
-            name ILIKE '%${filter}%'
+            name ILIKE '%${filter}%' 
+            AND  deleted_at IS NULL
         `
       }
 
@@ -85,18 +95,66 @@ export class TouristAttractionRepository
     }
   }
 
-  async create(TouristAttraction: TouristAttraction): Promise<void> {
+  async delete(touristAttraction: TouristAttraction): Promise<void> {
+    try {
+      const query = {
+        text: /* sql */ `
+          UPDATE tourist_attraction 
+          SET deleted_at = NOW() 
+          WHERE id = $1`,
+        values: [touristAttraction.id],
+      }
+
+      await this.pool.query(query)
+    } catch (error) {
+      console.error('Error deleting tourist attraction:', error)
+      throw error
+    }
+  }
+
+  async update(
+    touristAttraction: Optional<
+      OmitProps<TouristAttraction, 'cityId' | 'latitude' | 'longitude'>,
+      'name'
+    >,
+  ): Promise<void> {
+    try {
+      const namesToUpdate = ['id', 'description']
+      const values = [touristAttraction.id, touristAttraction.description]
+
+      if (touristAttraction.name) {
+        namesToUpdate.push('name')
+        values.push(touristAttraction.name)
+      }
+
+      const placeholders = namesToUpdate.map((_, i) => '$' + (i + 1)).join(', ')
+
+      const query = `
+        UPDATE 
+          tourist_attraction 
+        SET (${namesToUpdate.join(', ')}) = (${placeholders})  
+        WHERE 
+          id = $1`
+
+      await this.pool.query(query, values)
+    } catch (error) {
+      console.error('Error updating tourist attraction:', error)
+      throw error
+    }
+  }
+
+  async create(touristAttraction: TouristAttraction): Promise<void> {
     try {
       const query = {
         text: /* sql */ `
           INSERT INTO tourist_attraction (id, name, description, latitude, longitude) 
           VALUES ($1, $2, $3, $4, $5)`,
         values: [
-          TouristAttraction.id,
-          TouristAttraction.name,
-          TouristAttraction.description,
-          TouristAttraction.latitude.toString(),
-          TouristAttraction.longitude.toString(),
+          touristAttraction.id,
+          touristAttraction.name,
+          touristAttraction.description,
+          touristAttraction.latitude.toString(),
+          touristAttraction.longitude.toString(),
         ],
       }
 
