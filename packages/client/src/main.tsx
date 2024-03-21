332090ffb52046de742117eb1a6b8d2f7a63422e
Wagner Nascimento <54827039+WagnerNasc@users.noreply.github.com>
2024-03-21 18:05:01 -0300

    Update main.tsx

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -8,7 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="invoices" element={<Invoices />} /> */}
       </Routes>
     </BrowserRouter>
     ,
