bee896da616b0ececcd8764db9e12878f7425342
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-03-25 16:13:31 -0300

    feat: removed migration .js to .ts and added new migrations

--- packages/server/migrations/1711391655574_create-table-tourist-attraction.ts
+++ packages/server/migrations/1711391655574_create-table-tourist-attraction.ts
@@ -1,29 +1,29 @@
-/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate'
 
-exports.shorthands = undefined
+export const shorthands: ColumnDefinitions | undefined = undefined
 
-exports.up = (pgm) => {
+export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.createTable('tourist_attraction', {
     id: { type: 'uuid', primaryKey: true, notNull: true },
     city_id: {
       type: 'uuid',
       notNull: true,
       references: '"city"',
-      onDelete: 'cascade',
+      onDelete: 'CASCADE',
     },
     name: { type: 'varchar(255)', notNull: true },
     description: { type: 'text', notNull: true },
     latitude: { type: 'numeric(9,6)', notNull: true },
     longitude: { type: 'numeric(9,6)', notNull: true },
-    deleted_at: { type: 'timestamp' },
     updated_at: { type: 'timestamp' },
     created_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+    deleted_at: { type: 'timestamp' },
   })
 
-  pgm.createIndex('tourist_attraction', 'id', { unique: true })
   pgm.createIndex('tourist_attraction', 'city_id')
 }
 
-exports.down = (pgm) => {
+export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.dropTable('tourist_attraction')
 }
