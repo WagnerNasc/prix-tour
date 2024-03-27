b09b384317b765be919a9400951569ac147550a8
Wagner Nascimento <wagnerjr.nascimento10@gmail.com>
2024-03-27 09:54:11 -0300

    feat: added coverage

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -16,15 +16,20 @@ const config: Config = {
   coverageProvider: 'v8',
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/controllers/**/*.ts',
+    '!<rootDir>/src/controllers/**/*.ts', // TODO: e2e
     '!<rootDir>/src/env/**/*.ts',
     '!<rootDir>/src/helpers/**/*.ts',
     '!<rootDir>/src/infra/**/*.ts',
     '!<rootDir>/src/repositories/**.ts',
+    '!<rootDir>/src/repositories/interfaces/**.ts',
     '!<rootDir>/src/use-cases/errors/**/*.ts',
+    '!<rootDir>/src/use-cases/factories/**/*.ts',
+    '!<rootDir>/src/use-cases/interfaces/**/*.ts',
+    '!<rootDir>/src/test/**/*.ts',
     '!<rootDir>/src/main.ts',
     '!<rootDir>/src/routes.ts',
     '!<rootDir>/src/server.ts',
+    '!<rootDir>/src/app.ts',
   ],
 }
 
